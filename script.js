7b543c732730765e1185b04e8a45991ff9d31d7f
NomadicHippiee <84047650+NomadicHippiee@users.noreply.github.com>
2025-12-29 20:40:43 +0100

    Add gameboard

---
+++ script.js
@@ -0,0 +1,9 @@
+const Gameboard = (function(){
+    const board = [null, null, null, null, null, null, null, null, null];
+
+    return {
+        printBoard: function(){
+            console.log(board);
+        }
+    }
+})();
