2ba3399c01677dd276928a67a8008b89e1948a3b
NomadicHippiee <84047650+NomadicHippiee@users.noreply.github.com>
2025-12-29 21:53:21 +0100

    Add factory function for player

--- script.js
+++ script.js
@@ -4,6 +4,26 @@ const Gameboard = (function(){
     return {
         printBoard: function(){
             console.log(board);
+        },
+        placeMark: function(index, mark) {
+            if (board[index] === null) {
+                board[index] = mark;
+
+            } else {
+                return false;
+
+            }
+
         }
     }
 })();
+
+const Player = (name, mark) => {
+    let playerName = name;
+    let playerMark = mark;
+    return {
+        getName: () => playerName,
+        getMark: () => playerMark
+    }
+}
+
