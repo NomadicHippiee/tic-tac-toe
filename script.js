33411ca3783dd1630e6bf75ead81b0d733a32370
NomadicHippiee <84047650+NomadicHippiee@users.noreply.github.com>
2025-12-30 00:01:20 +0100

    Update gameController function

--- script.js
+++ script.js
@@ -1,29 +1,77 @@
-const Gameboard = (function(){
-    const board = [null, null, null, null, null, null, null, null, null];
+const Gameboard = (function () {
+  const board = [null, null, null, null, null, null, null, null, null];
 
-    return {
-        printBoard: function(){
-            console.log(board);
-        },
-        placeMark: function(index, mark) {
-            if (board[index] === null) {
-                board[index] = mark;
-
-            } else {
-                return false;
-
-            }
-
-        }
-    }
+  return {
+    printBoard: function () {
+      console.log(board);
+    },
+    placeMark: function (index, mark) {
+      if (board[index] === null) {
+        board[index] = mark;
+        return true;
+      } else {
+        return false;
+      }
+    },
+    getBoard: () => board,
+  };
 })();
 
 const Player = (name, mark) => {
-    let playerName = name;
-    let playerMark = mark;
-    return {
-        getName: () => playerName,
-        getMark: () => playerMark
-    }
-}
+  let playerName = name;
+  let playerMark = mark;
+  return {
+    getName: () => playerName,
+    getMark: () => playerMark,
+  };
+};
 
+const GameController = (function () {
+  const playerOne = Player("Player 1", "X");
+  const playerTwo = Player("Player 2", "O");
+  let currentPlayer = playerOne;
+
+  const winningCombinations = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  const checkWinner = function () {
+    const board = Gameboard.getBoard();
+    for (let combo of winningCombinations) {
+      const [a, b, c] = combo;
+      const spotA = board[a];
+      const spotB = board[b];
+      const spotC = board[c];
+
+      if (spotA !== null && spotA === spotB && spotB === spotC) {
+        return spotA;
+      }
+    }
+    return null;
+  };
+
+  return {
+    getCurrentPlayerName: () => currentPlayer.getName(),
+    getCurrentPlayerMark: () => currentPlayer.getMark(),
+    playTurn: function (index) {
+      const mark = currentPlayer.getMark();
+      const success = Gameboard.placeMark(index, mark);
+
+      if (success) {
+        if (currentPlayer === playerOne) {
+          currentPlayer = playerTwo;
+        } else {
+          currentPlayer = playerOne;
+        }
+      }
+    },
+    getWinner: () => checkWinner(),
+  };
+})();
